876b1cfa0efcf58a94f4bea74bfb383811ba5de4
KLiuli <1498181025@qq.com>
2020-09-12 17:23:02 +0800

    提交pom配置

---
+++ src/main/java/com/liuj/dao/IUserDao.java
@@ -0,0 +1,17 @@
+package com.liuj.dao;
+
+import com.liuj.domain.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 用户持久层接口
+ */
+public interface IUserDao {
+    /**
+     * 查询所有操作
+     */
+    @Select("select * from user")
+    List<User> findAll();
+}
